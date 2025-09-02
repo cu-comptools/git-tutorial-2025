9891342520e7680989ede7b40f1af18856bfdfe3
Sarah Kinney <saki9598@c3cpu-a2-u32-3.rc.int.colorado.edu>
2025-09-02 09:36:25 -0600

    Added unit test for chebD for n=1. Compared to analytical solution and asserted an error of less than 1E-14.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -12,4 +12,9 @@ def test_quadroots_1():
     assert (np.abs(roots[0] - x1_exact) < 1e-14) and (np.abs(roots[1] - x2_exact)) < 1e-14 
 
 def test_chebD_1():
+    n = 1
+    D,x = ch.chebD(n)
+    D_i1 = 0.5
+    D_i2 = -0.5
+    assert (np.abs(D[0,0] - D_i1) < 1e-14) and (np.abs(D[1,0] - D_i1) < 1e-14) and (np.abs(D[0,1] - D_i2) < 1e-14) and (np.abs(D[1,1] - D_i2) < 1e-14)
     pass
